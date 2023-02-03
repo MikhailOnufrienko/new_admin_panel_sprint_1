8881d8a98391ef9cfddc26c5c38d523f166e48dc
MikhailOnufrienko <ghast13@yandex.ru>
2023-02-03 22:04:24 +0400

    Add gettext_lazy functionality to models and mixins fields.

--- movies/mixins.py
+++ movies/mixins.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 import uuid
 
 
 class TimeStampedMixin(models.Model):
-    created = models.DateTimeField("Запись создана", auto_now_add=True)
-    modified = models.DateTimeField("Запись отредактирована", auto_now=True)
+    created = models.DateTimeField(_("Created"), auto_now_add=True)
+    modified = models.DateTimeField(_("Modified"), auto_now=True)
 
     class Meta:
         abstract = True
--- movies/models.py
+++ movies/models.py
@@ -1,28 +1,29 @@
 from django.db import models
 from .mixins import UUIDMixin, TimeStampedMixin
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class Genre(UUIDMixin, TimeStampedMixin):
-    name = models.TextField('Название')
-    description = models.TextField('Описание', blank=True)
+    name = models.TextField(_('Title'))
+    description = models.TextField(_('Description'), blank=True)
 
     class Meta:
         db_table = "content\".\"genre"
-        verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры'
+        verbose_name = _('Genre')
+        verbose_name_plural = _('Genres')
 
     def __str__(self):
         return self.name
 
 
 class Person(UUIDMixin, TimeStampedMixin):
-    full_name = models.TextField('Имя')
+    full_name = models.TextField(_('Name'))
 
     class Meta:
         db_table = "content\".\"person"
-        verbose_name = 'Персона'
-        verbose_name_plural = 'Персоны'
+        verbose_name = _('Person')
+        verbose_name_plural = _('People')
 
     def __str__(self):
         return self.full_name
@@ -31,19 +32,19 @@ class Person(UUIDMixin, TimeStampedMixin):
 class Filmwork(UUIDMixin, TimeStampedMixin):
 
     class Types(models.TextChoices):
-        MOVIE = 'movie', 'movie'
-        TV_SHOW = 'tv_show', 'tv_show'
+        MOVIE = 'movie', _('movie')
+        TV_SHOW = 'tv_show', _('tv_show')
 
-    title = models.TextField('Название')
-    description = models.TextField('Описание', blank=True)
-    creation_date = models.DateField('Дата создания фильма')
-    rating = models.FloatField('Рейтинг', blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
-    type = models.TextField('Тип', choices=Types.choices, default=Types.MOVIE)
+    title = models.TextField(_('Title'))
+    description = models.TextField(_('Description'), blank=True)
+    creation_date = models.DateField(_('Creation date'))
+    rating = models.FloatField(_('Rating'), blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    type = models.TextField(_('Type'), choices=Types.choices, default=Types.MOVIE)
 
     class Meta:
         db_table = "content\".\"film_work"
-        verbose_name = 'Фильм'
-        verbose_name_plural = 'Фильмы'
+        verbose_name = _('Movie')
+        verbose_name_plural = _('Movies')
 
     def __str__(self):
         return self.title
@@ -61,7 +62,7 @@ class GenreFilmwork(UUIDMixin):
 class PersonFilmwork(UUIDMixin):
     person = models.ForeignKey('Person', on_delete=models.CASCADE, verbose_name='Персона')
     film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE, verbose_name='Фильм')
-    role = models.TextField('Роль')
+    role = models.TextField(_('Role'))
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
