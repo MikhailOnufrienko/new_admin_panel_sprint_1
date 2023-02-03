027a61e15591f0e8b1cff3931b6f5625f3d264b2
MikhailOnufrienko <ghast13@yandex.ru>
2023-02-04 00:03:23 +0400

    Add fields 'certificate' and 'file' to model Filmwork.

--- movies/models.py
+++ movies/models.py
@@ -40,6 +40,8 @@ class Filmwork(UUIDMixin, TimeStampedMixin):
     creation_date = models.DateField(_('Creation date'))
     rating = models.FloatField(_('Rating'), blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
     type = models.TextField(_('Type'), choices=Types.choices, default=Types.MOVIE)
+    certificate = models.TextField(_('Certificate'), blank=True, null=True)
+    file_path = models.FileField(_('File'), blank=True, null=True, upload_to='movies/')
 
     class Meta:
         db_table = "content\".\"film_work"
