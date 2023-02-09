0ba4fd1649fc96c6d86a12da53345f82fdab5e9b
MikhailOnufrienko <ghast13@yandex.ru>
2023-02-09 23:57:19 +0400

    Change options in DB settings.
    Swap schemas: 'public,content'.

--- config/components/database.py
+++ config/components/database.py
@@ -10,7 +10,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', 5432),
         'OPTIONS': {
-            'options': '-c search_path=content,public'
+            'options': '-c search_path=public,content'
         }
     }
 }
